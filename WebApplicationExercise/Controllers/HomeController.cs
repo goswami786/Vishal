5e3ef03223556d8a1f17108400003ec9511ca013
goswami786 <vishalgoswami2116@gmail.com>
2020-12-04 16:57:33 +0530

    third commit

--- WebApplicationExercise/Controllers/HomeController.cs
+++ WebApplicationExercise/Controllers/HomeController.cs
@@ -27,6 +27,17 @@ namespace WebApplicationExercise.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
