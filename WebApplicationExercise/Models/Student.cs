d21f84b7e70f768af2a070c4a493c83e009e40d8
goswami786 <vishalgoswami2116@gmail.com>
2020-12-04 16:25:27 +0530

    initial commit

---
+++ WebApplicationExercise/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplicationExercise.Models
+{
+    public class Student
+    {
+        public int SId { get; set; }
+        public String SName { get; set; }
+        public DateTime DOB { get; set; }
+        public double SContact { get; set; }
+    }
+}
